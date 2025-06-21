983857944c8736b6661b11ec53ec188a21e3a00b
frusianblue <frusianblue@gmail.com>
2025-06-22 01:58:38 +0900

    세번째 커밋

--- src/main/java/com/quickstart/guide/GuideApplication.java
+++ src/main/java/com/quickstart/guide/GuideApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuideApplication {
 	
+	// 네번째 커밋 테스트
+	// 세번째 커밋 테스트
+
 	// frucyan 두번째 커밋 테스트
 	// frucyan 계정 커밋 테스트
 	// frusianblue  두번째 커밋 테스트
