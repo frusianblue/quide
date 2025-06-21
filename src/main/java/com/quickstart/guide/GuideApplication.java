ad772acb5e0fd5a91341359bdc775010c0ee87d7
frucyan <frucyan@gmail.com>
2025-06-22 04:14:18 +0900

    frucyan Change

--- src/main/java/com/quickstart/guide/GuideApplication.java
+++ src/main/java/com/quickstart/guide/GuideApplication.java
@@ -6,19 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuideApplication {
 
-	// frusianblue 커밋 메세지
-	// frusianblue 두번째 메세지
-	// frrusianble 세번째 테스트
-	// frrusianble 네번째 테스트
-	
-	// frucyan 커밋 테스트
-	// frucyan 테스트2
-	// frucyan 테스트3
-	// frucyan 테스트4
-	// frucyan 테스트5
-	// frucyan 테스트6
-	
-	// 그냥 테스트
+	// frucyan
+	// 테스트중
 
 	// 주석 테스트
 	public static void main(String[] args) {
