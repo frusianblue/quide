e9fe60f91e3164c6bb2fa7e1682392c789267d9a
frusianblue <frusianblue@gmail.com>
2025-06-22 10:29:20 +0900

    Changes

--- src/main/java/com/quickstart/guide/GuideApplication.java
+++ src/main/java/com/quickstart/guide/GuideApplication.java
@@ -5,13 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GuideApplication {
-	// frusianblue
-	// 테스트1
-	// myTest, chae_ohsik, flasOh,
 
-	// frucyan
-	// 테스트중
-	// 테스트 2
 
 	// 주석 테스트
 	public static void main(String[] args) {
