d04f2140356dad32f46fa559baf52db7a80fdc5b
frusianblue <frusianblue@gmail.com>
2025-06-22 13:52:42 +0900

    Change

--- src/main/java/com/quickstart/guide/GuideApplication.java
+++ src/main/java/com/quickstart/guide/GuideApplication.java
@@ -6,25 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuideApplication {
 
-	// reset 테스트1
-	// reset 테스트2
-
-
-	// feture-test01 테스트
-
-	
-	
-	// feture-test02 테스트
-	// 내용도 넣어 볼까
-	
-	
-	// feature-test03
-	// 이번에는 close를 해본다
-	
-	// feature-test04
-	// 이번에는 Pull Request 되나 해볼까
-	
-	// develop 테스트
+	// 테스트 많이 했다
 	
 	// 주석 테스트
 	public static void main(String[] args) {
