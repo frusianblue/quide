6270588a5af69b37506c27ece658eab2221bc8f7
frusianblue <frusianblue@gmail.com>
2025-06-22 11:23:14 +0900

    Changes

--- src/main/java/com/quickstart/guide/GuideApplication.java
+++ src/main/java/com/quickstart/guide/GuideApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuideApplication {
 	
+	// Merge 테스트
+	
 	// 주석 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(GuideApplication.class, args);
