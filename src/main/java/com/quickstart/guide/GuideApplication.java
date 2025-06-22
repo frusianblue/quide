cc93cd9ab1517edab3ff18d6063a070ae6616fc4
frusianblue <frusianblue@gmail.com>
2025-06-22 11:40:57 +0900

    Changes

--- src/main/java/com/quickstart/guide/GuideApplication.java
+++ src/main/java/com/quickstart/guide/GuideApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GuideApplication {
 	
 	// Merge는 이렇게 feture 를 develop에 merge할때 사용 하나
-
+	
+	// 병합 잘되네
+	
 	// 주석 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(GuideApplication.class, args);
