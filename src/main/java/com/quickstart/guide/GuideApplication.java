75dd62ee9e9c7b32e83f639cc210461d2e72b068
frusianblue <frusianblue@gmail.com>
2025-06-22 12:11:54 +0900

    Changes

--- src/main/java/com/quickstart/guide/GuideApplication.java
+++ src/main/java/com/quickstart/guide/GuideApplication.java
@@ -8,6 +8,8 @@ public class GuideApplication {
 	
 	// feture-test01 테스트
 
+	// feture-test02 테스트
+
 	// 주석 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(GuideApplication.class, args);
